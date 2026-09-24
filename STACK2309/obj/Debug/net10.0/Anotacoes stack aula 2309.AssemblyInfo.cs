7df18b5250df18e6feee7cff765f195bd8372a11
Henrique <henriquepauluk00@gmail.com>
2026-09-24 12:28:58 -0300

    CERTO

--- STACK2309/obj/Debug/net10.0/Anotacoes stack aula 2309.AssemblyInfo.cs
+++ STACK2309/obj/Debug/net10.0/Anotacoes stack aula 2309.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Anotacoes stack aula 2309")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750599418691ce6c23235c778233a2b6bd934a50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Anotacoes stack aula 2309")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Anotacoes stack aula 2309")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- VETORES/obj/Debug/net10.0/VETORES.AssemblyInfo.cs
+++ VETORES/obj/Debug/net10.0/VETORES.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VETORES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750599418691ce6c23235c778233a2b6bd934a50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VETORES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VETORES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
